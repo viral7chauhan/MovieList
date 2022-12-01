fa93cfa21b0df56f8e69e15e7c6b7fe86b1b1d99
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-01 11:59:38 +0530

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Viral on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
