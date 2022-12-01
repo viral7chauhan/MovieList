227a4368c21c35ed6536929c915357104b337797
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-01 17:26:07 +0530

    Give url while calling RemoteMovieLoader

--- MovieListTests/RemoteMovieLoaderTests.swift
+++ MovieListTests/RemoteMovieLoaderTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteMovieLoader {
     let client: HTTPClient
+    let url: URL
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "a-hrl.com")!)
+        client.get(from: url)
     }
 }
 
@@ -33,18 +35,20 @@ class HTTPClientSpy: HTTPClient {
 
 final class RemoteMovieLoaderTests: XCTestCase {
     func test_init_doesNotRequestData() {
+        let url = URL(string: "http://another-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteMovieLoader(client: client)
+        _ = RemoteMovieLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_init_requestDataFromURL() {
+        let url = URL(string: "http://another-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteMovieLoader(client: client)
+        let sut = RemoteMovieLoader(url: url, client: client)
 
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
