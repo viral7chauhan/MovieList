ef65e1c1f1c96a3693dae838aff25bbdb967960e
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-01 22:24:29 +0530

    Improve test spy capturing

--- MovieListTests/RemoteMovieLoaderTests.swift
+++ MovieListTests/RemoteMovieLoaderTests.swift
@@ -36,10 +36,12 @@ final class RemoteMovieLoaderTests: XCTestCase {
 
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "test", code: 0)
 
         var capturedErrors = [RemoteMovieLoader.Error]()
         sut.load { capturedErrors.append($0) }
+
+        let clientError = NSError(domain: "test", code: 0)
+        client.completion(with: clientError)
 
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -54,14 +56,19 @@ final class RemoteMovieLoaderTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var error: Error?
+        private var messages = [(url: URL,
+                                completion: (Error) -> Void)]()
+
+        var requestedURLs: [URL] {
+            messages.map(\.url)
+        }
 
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
-            requestedURLs.append(url)
+            messages.append((url, completion))
+        }
+
+        func completion(with error: Error, at index: Int = 0) {
+            messages[index].completion(error)
         }
     }
 }
