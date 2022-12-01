47ab39f2f2461fb15dff825fccdd59520bcc7c42
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-01 17:36:26 +0530

    Make SUT helper method

--- MovieListTests/RemoteMovieLoaderTests.swift
+++ MovieListTests/RemoteMovieLoaderTests.swift
@@ -25,30 +25,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class RemoteMovieLoaderTests: XCTestCase {
     func test_init_doesNotRequestData() {
-        let url = URL(string: "http://another-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteMovieLoader(url: url, client: client)
+        let (_ , client) = makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_init_requestDataFromURL() {
-        let url = URL(string: "http://another-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteMovieLoader(url: url, client: client)
+        let givenURL = URL(string: "https://another-url.com")!
+        let (sut, client) = makeSUT(url: givenURL)
 
         sut.load()
 
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURL, givenURL)
+    }
+
+    // MARK: - Helper
+    private func makeSUT(url: URL = URL(string: "http://another-url.com")!)
+    -> (sut: RemoteMovieLoader, client: HTTPClientSpy)
+     {
+        let client = HTTPClientSpy()
+        let sut = RemoteMovieLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
