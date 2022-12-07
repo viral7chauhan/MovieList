3073cdcec3df496f31be0078ae0271af4afa22d5
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-07 16:35:26 +0530

    Implement image handling in ImageViewModel

--- MovieList/ViewModel/MovieImageViewModel.swift
+++ MovieList/ViewModel/MovieImageViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class MovieImageViewModel {
 
@@ -21,5 +22,29 @@ final class MovieImageViewModel {
 
     var title: String { model.title }
     var overview: String { model.overview }
-    
+    var releaseDate: String { "\(model.releaseDate)" }
+
+    var onImageLoad: Observer<UIImage>?
+
+    func loadImageData() {
+        guard let url = URL(string: model.thumbnailImage) else {
+            return
+        }
+        task = imageLoader.loadImageData(from: url) { [weak self] result in
+            self?.handle(result)
+        }
+    }
+
+    private func handle(_ result: MovieImageDataLoader.Result) {
+        if let data = (try? result.get()),
+           let image = UIImage(data: data) {
+            onImageLoad?(image)
+        }
+    }
+
+    func cancelImageDataLoad() {
+        task?.cancel()
+        task = nil
+    }
+
 }
