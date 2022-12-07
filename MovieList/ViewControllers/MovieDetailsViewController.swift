b2e0bbff0c01929d9beb2af147b8a9b53cd13938
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-07 22:43:57 +0530

    Create MovieDetailsViewController

---
+++ MovieList/ViewControllers/MovieDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsViewController.swift
+//  MovieList
+//
+//  Created by Viral on 07/12/22.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
